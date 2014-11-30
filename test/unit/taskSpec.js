01d6f666ca95e9ea2fd9d0260bc8b81a617d7c98
Andrew Revinsky <arevinsky@gmail.com>
2014-11-30 05:12:53 +0400

    Unit tests

---
+++ test/unit/taskSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by andrew on 11/30/14.
+ */
+describe('TaskCruncherPresence', function(){
+
+	//beforeEach(module('phonecatApp'));
+
+	it('exists', function(){
+		expect(Task).not.toBeUndefined();
+	});
+
+});
