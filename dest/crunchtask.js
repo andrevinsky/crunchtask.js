e8e27d9983050a90345cdcb5333543a915540675
Andrew Revinsky <arevinsky@gmail.com>
2015-02-14 20:46:43 +0300

    Build set up, version bump

--- dest/crunchtask.js
+++ dest/crunchtask.js
@@ -184,6 +184,18 @@
 		});
 	};
 })();
+// should work in any browser without browserify
+
+if (typeof Promise.prototype.done !== 'function') {
+  Promise.prototype.done = function (onFulfilled, onRejected) {
+    var self = arguments.length ? this.then.apply(this, arguments) : this
+    self.then(null, function (err) {
+      setTimeout(function () {
+        throw err
+      }, 0)
+    })
+  }
+}
 /**
  * Created by ANDREW on 10/10/2014.
  * @requires 'bower_components/promise-polyfill'
